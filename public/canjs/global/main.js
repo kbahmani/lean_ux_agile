6dd46a8419500a2ca192fd0515aa42b985e00d52
kia <kbahmani@mudynamics.com>
2013-03-04 12:00:59 -0800

    Cleanup

--- public/canjs/global/main.js
+++ public/canjs/global/main.js
@@ -1,5 +1,5 @@
 steal(
-    'global/lib/jquery-1.7.2.min.js'
+    'global/lib/jquery-1.7.2.js'
 ).then('global/lib/can.jquery-1.0.7.min.js')
 .then(
     'global/lib/jquery-ui-1.8.15.min.js',
@@ -23,9 +23,6 @@ steal(
 
         window.app_state = new can.Observe(JSON.parse(old_state));
         app_state.bind( 'change', function( ev, attr, how, newVal, oldVal ) {
-//           console.info(ev)
-//           console.info(attr+" / "+how+" / "+newVal+" / "+oldVal);
-//           console.info(window.app_state._data.valueOf())
            window.sessionStorage["app_state"] = JSON.stringify(window.app_state._data.valueOf());
         });
     }
@@ -42,32 +39,8 @@ steal(
         handle_route: function(data){
             var widget = data['widget'];
             var sub = data['sub'];
-
             var self = this;
-
-            if(widget === 'account') {
-                self.cleanElement('contents');
-                var Account = new Global.App.AccountCtrl("#contents");
-                if(sub === 'load_users') {
-                    Account.load_users(widget);
-                }
-            } else if(widget === 'fuzzProfile') {
-                if((app_state.route && app_state.route.widget !== 'fuzzProfile') || !sub){ 
-                    self.cleanElement('contents');
-                    $("#contents").append("<div id='contents_inner'></div>")
-                }
-                $("#contents").profile_builder();
-                if(sub) {
-                    self.cleanElement('contents_inner');
-                    $("#contents_inner").tab_builder();
-                }
-            } else if(widget === 'fuzzPlayer') {
-                self.cleanElement('contents');
-                var fuzzPlayer = new Fuzz.App.FuzzPlayer("#contents");
-            } else if(widget === 'fuzzLibrary') {
-                self.cleanElement('contents');
-                var Library = new Fuzz.App.FuzzLibrary("#contents");
-            }
+						
             app_state.attr('route', {'widget': widget, 'sub': sub});
         },
         cleanElement: function(element_id) {
@@ -83,30 +56,9 @@ steal(
 })
 .then(
 	'global/lib/can.fixture.js',
-    'global/lib/can.construct.proxy.js',
-    'global/lib/can.construct.super.js',
-    'global/lib/can.control.plugin.js',
-    'global/lib/can.control.view.js',
-    'global/lib/can.view.modifiers.js',
-	'global/css/global.css',
-	'global/app/layout'
+	'global/lib/can.construct.proxy.js',
+	'global/lib/can.construct.super.js',
+	'global/lib/can.control.plugin.js',
+	'global/lib/can.control.view.js',
+	'global/lib/can.view.modifiers.js'
 )
-
-function enableErrorMessage(flag) {
-    if(flag) {
-        $("#message").bind("ajaxError", function(e, jqXHR, settings, exception) {
-            $(".spinnerbig").hide();
-            var error_message = "";
-            try {
-                error_message = $.parseJSON(jqXHR.responseTest);
-            } catch(e) {
-                error_message = e;
-                error_message["success"] = false;
-            }
-
-            new Global.App.Message("#message");
-        });
-    } else {
-        $("#message").unbind("ajaxError");
-    }
-}
